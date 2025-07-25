4daa043eec854721c28b95d4e051ea01f52ec963
Anit Padalkar <apadalkar@berkeley.edu>
2025-07-25 10:28:31 -0700

    added hand landmarks and connections in video feed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import cv2
 import time
 from gesture_recognition import recognize_gesture
+from gesture_recognition import mp_drawing, mp_hands, hands_detector
 
 OUTPUT_HZ = 10
 
@@ -12,8 +13,18 @@ if __name__ == "__main__":
             if not ret:
                 print("Failed to grab frame")
                 break
-            command = recognize_gesture(frame)
-            print(f"Command: {command}")
+            # Mirror the frame 
+            frame = cv2.flip(frame, 1)
+            # run gesture recognition and get landmarks
+            image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = hands_detector.process(image_rgb)
+            command = None
+            if results.multi_hand_landmarks:
+                command = recognize_gesture(frame)
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            print("Command: ", command)
             cv2.imshow('Gesture Recognition - Press q to quit', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
