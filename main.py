c2ddfb2a0a617c7ce657f70690776fce9de5e9da
Anit Padalkar <apadalkar@berkeley.edu>
2025-07-25 10:12:45 -0700

    Added hand detection to return 'hover' if a hand is detected and None otherwise

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import cv2
+import time
+from gesture_recognition import recognize_gesture
+
+OUTPUT_HZ = 10
 
 if __name__ == "__main__":
     cap = cv2.VideoCapture(0)
@@ -8,9 +12,12 @@ if __name__ == "__main__":
             if not ret:
                 print("Failed to grab frame")
                 break
-            cv2.imshow('Webcam Test - Press q to quit', frame)
+            command = recognize_gesture(frame)
+            print(f"Command: {command}")
+            cv2.imshow('Gesture Recognition - Press q to quit', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+            time.sleep(1 / OUTPUT_HZ)
     finally:
         cap.release()
         cv2.destroyAllWindows() 
